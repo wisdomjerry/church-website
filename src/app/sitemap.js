ae5230f61328ab49c1d55394f65179b13566e2ef
wisdomjerry <wisdom.jeremiah.upti@gmail.com>
2026-01-24 16:45:53 +0300

    add google console verification

---
+++ src/app/sitemap.js
@@ -0,0 +1,12 @@
+export default function sitemap() {
+  return [
+    { url: "https://https://christainfaithharvestchurch.vercel.app/", lastModified: new Date() },
+    { url: "https://christainfaithharvestchurch.vercel.app/about", lastModified: new Date() },
+    { url: "https://christainfaithharvestchurch.vercel.app/events", lastModified: new Date() },
+    { url: "https://christainfaithharvestchurch.vercel.app/contact", lastModified: new Date() },
+    { url: "https://christainfaithharvestchurch.vercel.app/sermon", lastModified: new Date() },
+    { url: "https://christainfaithharvestchurch.vercel.app/blog", lastModified: new Date() },
+    { url: "https://christainfaithharvestchurch.vercel.app/donate", lastModified: new Date() },
+    { url: "https://christainfaithharvestchurch.vercel.app/ministries", lastModified: new Date() },
+  ];
+}
